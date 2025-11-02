ee1a63bd75a0d6d856a4fcf20d2013d740d02130
naveen sanjula <82176749+naveensanjula975@users.noreply.github.com>
2025-11-02 11:44:49 +0530

    feat(db): add initial EF Core migration for Books table

---
+++ backend/Migrations/20251102061325_InitialCreate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Author = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 500, nullable: true),
+                    PublishedYear = table.Column<int>(type: "INTEGER", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "Author", "CreatedAt", "Description", "PublishedYear", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Michael Hartl", new DateTime(2025, 11, 2, 0, 0, 0, 0, DateTimeKind.Utc), "An Introduction to Version Control with Git", 2016, "Learn Enough Git to Be Dangerous" },
+                    { 2, "Keyvan Kambakhsh", new DateTime(2025, 11, 2, 0, 0, 0, 0, DateTimeKind.Utc), "A Colorful Introduction to Engineering!", 2024, "The Super Programmer" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
